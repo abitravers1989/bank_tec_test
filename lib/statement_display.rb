bc5900d386cd7d236d9796aef92d94e5ed9c8d06
Abi Travers <atravers4007@gmail.com>
2017-10-03 14:14:44 +0100

    splitting out display method

--- lib/statement_display.rb
+++ lib/statement_display.rb
@@ -2,9 +2,12 @@ require 'account'
 # this class is for storing the statement
 
 class Statement_Display
+
+  attr_accessor :string
+
   def initialize(transactioninfo)
     @transactioninfo = transactioninfo
-    @string = zero
+    @string = []
   end
 
   def printstatmentheader
@@ -16,11 +19,17 @@ class Statement_Display
   # end
   #
 
-  def display
+  def displaysetting
     @transactioninfo.each do |y|
-      puts y[:date].to_s + ' || ' + y[:credit].to_s + ' || ' + y[:debit].to_s + ' || ' + y[:balance].to_s
+      @string =  y[:date].to_s + y[:credit].to_s + y[:debit].to_s + y[:balance].to_s
     end
-end
+  end
+
+  def display
+    displaysetting
+    # puts @string.split().join("||")
+  end
+
 end
 
 statement = Statement_Display.new([{:credit=>2000, :balance=>2000, :date=>"03/10/2017"}, {:debit=>1000, :balance=>1000, :date=>"03/10/2017"}])
