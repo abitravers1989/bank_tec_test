7ec6ade6f7643d0201089532fa6bc36c427e0f32
Abi Travers <atravers4007@gmail.com>
2017-10-04 15:11:00 +0100

    Complexity of Statement_dsiplay class methods reduced down to below 5

--- lib/statement_display.rb
+++ lib/statement_display.rb
@@ -14,7 +14,7 @@ class Statement_Display
   end
 
   def stringintepolation(n)
-      "#{n[:date]} || #{n[:credit]} || #{n[:debit]} || #{n[:balance]}"
+    "#{n[:date]} || #{n[:credit]} || #{n[:debit]} || #{n[:balance]}"
   end
 
   def display
@@ -22,7 +22,6 @@ class Statement_Display
       puts stringintepolation(y)
     end
   end
-
 end
 
 statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
--- spec/statment_display_spec.rb
+++ spec/statment_display_spec.rb
@@ -3,7 +3,6 @@ require 'account'
 require 'spec_helper'
 
 describe Statement_Display do
-
   context '#printstatmentheader' do
     it 'prints the statement header' do
       statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
@@ -14,22 +13,22 @@ describe Statement_Display do
   context '#display' do
     it 'displays the transaction info to standard output' do
       statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }])
-      expect { statement.display }.to output("03/10/2017 || 2000 ||  || 2000 || \n").to_stdout
+      expect { statement.display }.to output("03/10/2017 || 2000 ||  || 2000\n").to_stdout
     end
   end
 end
 
-  # context '#formattingarrayvalues' do
-  #   it 'converts a value to a string and adds || to the end of it' do
-  #       statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
-  #     expect { statement.formattingarrayvalues(2000) }.to output("2000 ||").to_stdout
-  #   end
-  # end
+# context '#formattingarrayvalues' do
+#   it 'converts a value to a string and adds || to the end of it' do
+#       statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
+#     expect { statement.formattingarrayvalues(2000) }.to output("2000 ||").to_stdout
+#   end
+# end
 
-  # context '#displaysetting' do
-  #   it 'puts array of values into string variable' do
-  #     statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
-  #     statement.displaysetting
-  #     expect (statement.string).to be_instance_of Array
-  #   end
-  # end
+# context '#displaysetting' do
+#   it 'puts array of values into string variable' do
+#     statement = Statement_Display.new([{ credit: 2000, balance: 2000, date: '03/10/2017' }, { debit: 1000, balance: 1000, date: '03/10/2017' }])
+#     statement.displaysetting
+#     expect (statement.string).to be_instance_of Array
+#   end
+# end
